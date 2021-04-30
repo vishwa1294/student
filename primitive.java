1f033f35594ad8810674f5c72fb6d3998aa618ba
vishwa1294 <vishwanathjatti@gmail.com>
2021-04-30 18:53:58 +0530

    primitive.java

---
+++ primitive.java
@@ -0,0 +1,30 @@
+class Primitive1
+{
+public static void main(String[]args)
+{
+byte myNum=100;
+System.out.println(myNum);
+short number=3000;
+System.out.println(number);
+
+int number2=200000;
+System.out.println(number2);
+
+long number3=3540000000L;
+System.out.println(number3);
+
+float number4=3.87f;
+System.out.println(number4);
+
+double number5=56.89d;
+System.out.println(number5);
+
+char myName='v';
+System.out.println(myName);
+ 
+boolean study=true;
+boolean noStudy=false;
+System.out.println(study);
+System.out.println(noStudy);
+}
+}
